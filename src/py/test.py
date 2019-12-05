5a4b39424f6a3d49d31f6b375a4dbe22d12720fa
SYury <yusemru@gmail.com>
2019-12-05 11:56:36 +0300

    тесты

--- src/py/test.py
+++ src/py/test.py
@@ -1,99 +1,126 @@
+import numpy as np
 import torch
-from torch.optim.adagrad import Adagrad
+from torch.autograd import Variable
+from torch.nn import functional as F
+from torch.optim.adam import Adam
+from torch.optim.sgd import SGD
 from torch.optim.lbfgs import LBFGS
 from a2grad import A2Grad
 
-methods = [
-        'A2Grad-uni',
-        'A2Grad-inc',
-        'A2Grad-exp',
-        'Adagrad', # быстрый градиентный метод
-        'L-BFGS' # квазиньютоновский метод
-]
+class LinearRegression(torch.nn.Module):
+    def __init__(self):
+        super(LinearRegression, self).__init__()
+        self.linear = torch.nn.Linear(1, 1)
+    def forward(self, x):
+        y_pred = self.linear(x)
+        return y_pred
+
+class RegressionTest:
+    def __init__(self, name, model, create_opt, criterion, x, y):
+        self.name = name
+        self.model = model
+        self.opt = create_opt(self.model)
+        self.criterion = criterion
+        self.x = x
+        self.y = y
+        self.errors = []
+    
+    def epoch(self):
+        x_v = Variable(torch.from_numpy(self.x))
+        y_v = Variable(torch.from_numpy(self.y))
+        self.opt.zero_grad()
+        y_pred = self.model(x_v)
+        loss = self.criterion(y_v, y_pred)
+        self.errors.append(loss)
+        loss.backward()
+        self.opt.step(lambda : 0)
+    
+    def output(self, epochs):
+        s = self.name + ' '
+        for e in epochs:
+            s += "{} ".format(self.errors[e])
+        print(s)
 
 def test_linreg():
     print('Test: linear regression')
-    N = 3
-    M = 10
-    x = torch.randn((M, N))
-    real_A = torch.randn((N, 1))
-    y = x.mm(real_A) + torch.randn(M)
-    A = torch.randn((N, 1), requires_grad=True)
     
-    vec = [A, A, A, A, A]
+    x_values = []
+    y_values = []
     
-    optimizer = [
-        A2Grad([vec[0]], 'uni'),
-        A2Grad([vec[1]], 'inc'),
-        A2Grad([vec[2]], 'exp'),
-        Adagrad([vec[3]]),
-        LBFGS([vec[4]])
+    for i in range(20):
+        x_values.append(i)
+        y_values.append(5*i + 2 + torch.randn(1)[0])
+    
+    x_data = np.array(x_values, dtype=np.float32).reshape(-1, 1)
+    y_data = np.array(y_values, dtype=np.float32).reshape(-1, 1)
+    
+    test = [
+        RegressionTest('A2Grad-uni', LinearRegression(), lambda model : A2Grad(model.parameters(), 'uni'), torch.nn.MSELoss(), x_data, y_data), 
+        RegressionTest('A2Grad-inc', LinearRegression(), lambda model : A2Grad(model.parameters(), 'inc'), torch.nn.MSELoss(), x_data, y_data),
+        RegressionTest('A2Grad-exp', LinearRegression(), lambda model : A2Grad(model.parameters(), 'exp'), torch.nn.MSELoss(), x_data, y_data),
+        RegressionTest('Adam', LinearRegression(), lambda model : Adam(model.parameters()), torch.nn.MSELoss(), x_data, y_data),
+        RegressionTest('SGD', LinearRegression(), lambda model : SGD(model.parameters(), lr=1e-2), torch.nn.MSELoss(), x_data, y_data),
+        RegressionTest('LBFGS', LinearRegression(), lambda model : LBFGS(model.parameters()), torch.nn.MSELoss(), x_data, y_data)
         ]
-
-    errors = [[], [], [], [], []]
+    
+    for i in range(len(test)):
+        for n in range(52):
+            test[i].epoch()
     
     points = [10, 20, 30, 40, 50]
     header = "method   "
     for i in points:
         header += "{}                   ".format(i)
     print(header)
-    for n in range(1, 52):
-        for i in range(len(methods)):
-            optimizer[i].zero_grad()
-            mse = torch.mean((x.mm(vec[i]) - y)**2)
-            if n - 1 in points:
-                errors[i].append(mse)
-            mse.backward()
-            optimizer[i].step(lambda : 0)
-    
-    for i in range(len(methods)):
-        s = methods[i] + ' '
-        for err in errors[i]:
-            s += "{} ".format(err)
-        print(s)
+    for i in range(len(test)):
+        test[i].output(points)
     print('')
 
-def test_normsquare():
-    print("Test: (1, 2, 3)^T*x + 2||x - (0, 1, 0)||^2")
-    c = torch.tensor([1.0, 2.0, 3.0])
-    xk = torch.tensor([0.0, 1.0, 0.0])
-    x = torch.randn(3, requires_grad=True)
+class LogisticRegression(torch.nn.Module):
+     def __init__(self):
+        super(LogisticRegression, self).__init__()
+        self.linear = torch.nn.Linear(1, 1)
+     def forward(self, x):
+        y_pred = F.sigmoid(self.linear(x))
+        return y_pred
 
-    vec = [x, x, x, x, x]
+def test_logreg():
+    print('Test: logistic regression')
     
-    optimizer = [
-        A2Grad([vec[0]], 'uni'),
-        A2Grad([vec[1]], 'inc'),
-        A2Grad([vec[2]], 'exp'),
-        Adagrad([vec[3]]),
-        LBFGS([vec[4]])
+    x_values = []
+    y_values = []
+    
+    for i in range(20):
+        x_values.append(i)
+        y_values.append(5*i + 2 + torch.randn(1)[0])
+    
+    x_data = np.array(x_values, dtype=np.float32).reshape(-1, 1)
+    y_data = np.array(y_values, dtype=np.float32).reshape(-1, 1)
+    
+    test = [
+        RegressionTest('A2Grad-uni', LogisticRegression(), lambda model : A2Grad(model.parameters(), 'uni'), torch.nn.MSELoss(), x_data, y_data), 
+        RegressionTest('A2Grad-inc', LogisticRegression(), lambda model : A2Grad(model.parameters(), 'inc'), torch.nn.MSELoss(), x_data, y_data),
+        RegressionTest('A2Grad-exp', LogisticRegression(), lambda model : A2Grad(model.parameters(), 'exp'), torch.nn.MSELoss(), x_data, y_data),
+        RegressionTest('Adam', LogisticRegression(), lambda model : Adam(model.parameters()), torch.nn.MSELoss(), x_data, y_data),
+        RegressionTest('SGD', LogisticRegression(), lambda model : SGD(model.parameters(), lr=1e-2), torch.nn.MSELoss(), x_data, y_data),
+        RegressionTest('LBFGS', LogisticRegression(), lambda model : LBFGS(model.parameters()), torch.nn.MSELoss(), x_data, y_data)
         ]
-
-    vals = [[], [], [], [], []]
+    
+    for i in range(len(test)):
+        for n in range(52):
+            test[i].epoch()
     
     points = [10, 20, 30, 40, 50]
     header = "method   "
     for i in points:
         header += "{}                   ".format(i)
     print(header)
-    for n in range(1, 52):
-        for i in range(len(methods)):
-            optimizer[i].zero_grad()
-            val = torch.dot(c, vec[i]) + 2*torch.dist(vec[i], xk)**2
-            if n - 1 in points:
-                vals[i].append(val)
-            val.backward()
-            optimizer[i].step(lambda : 0)
-    
-    for i in range(len(methods)):
-        s = methods[i] + ' '
-        for val in vals[i]:
-            s += "{} ".format(val)
-        print(s)
+    for i in range(len(test)):
+        test[i].output(points)
     print('')
 
 def main():
     test_linreg()
-    test_normsquare()
+    test_logreg()
 
 main()
